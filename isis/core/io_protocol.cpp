047518bb7b398792967c92522610dd3d43a885c3
Enrico Reimer <reimer@cbs.mpg.de>
2022-07-17 21:52:39 +0200

    add base protocol class and proof of concept implementation of DirectoryProtocol

--- isis/core/io_protocol.cpp
+++ isis/core/io_protocol.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "io_protocol.hpp"
+#include "fileptr.hpp"
+#include <fstream>
+
+namespace isis::io{
+
+util::slist DirectoryProtocol::prefixes(){return {"file:"};}
+util::slist DirectoryProtocol::suffixes(){return {};}
+
+Generator<IoProtocol::load_result> DirectoryProtocol::load(std::string path)
+{
+	for(auto f=std::filesystem::directory_iterator( path );f!=std::filesystem::directory_iterator();++f)
+	{
+		if(f->is_directory())continue;
+		if(f->file_size()>mapping_size) {
+			data::FilePtr memory(f->path());
+			co_yield load_result{f->path().native(),memory};
+		} else {
+			std::filebuf stream;
+			stream.open(f->path(),std::ios_base::binary|std::ios_base::in);
+			co_yield load_result{f->path().native(),std::move(stream)};
+		}
+	}
+}
+}
--- isis/core/io_protocol.hpp
+++ isis/core/io_protocol.hpp
@@ -3,14 +3,75 @@
 //
 
 #pragma once
-
+#ifdef __clang__
+	#include <experimental/coroutine>
+	using std::experimental::coroutine_handle;
+	using std::experimental::suspend_never;
+	using std::experimental::suspend_always;
+#else
+	#include <coroutine>
+	using std::coroutine_handle;
+	using std::suspend_never;
+	using std::suspend_always;
+#endif
+#include "bytearray.hpp"
 #include "common.hpp"
 #include "types.hpp"
 
 namespace isis::io
 {
+
+template<typename T> struct Generator {
+	struct promise_type;
+	using handle_type = coroutine_handle<promise_type>;
+
+	struct promise_type { // required
+		T value_;
+		std::exception_ptr exception_;
+
+		Generator get_return_object() {return Generator(handle_type::from_promise(*this));}
+		suspend_never initial_suspend() {return {};}
+		suspend_always final_suspend() noexcept {return {};}
+		void unhandled_exception() { exception_ = std::current_exception(); } // saving exception
+
+		template<class From> suspend_always yield_value(From &&from) {
+			value_ = std::move(from); // caching the result in promise
+			return {};
+		}
+		void return_void() {}
+	};
+
+	handle_type h_;
+
+	Generator(handle_type h) : h_(h) {}
+	~Generator() { h_.destroy(); }
+	explicit operator bool(){
+		return !h_.done();
+	}
+	T operator()() {
+		T ret= std::move(h_.promise().value_);
+		h_.resume();
+		return ret;
+	}
+};
+
+
 class IoProtocol
 {
+public:
+	typedef std::variant<std::filebuf, data::ByteArray> io_object;
+	typedef std::tuple<std::string,io_object> load_result;
+	virtual Generator<load_result> load(std::string path) = 0;
 	virtual util::slist prefixes() = 0;
+	virtual util::slist suffixes() = 0;
+
+};
+
+class DirectoryProtocol : public IoProtocol{
+	static const size_t mapping_size = 1024*1024*10;
+public:
+	util::slist prefixes() override;
+	util::slist suffixes() override;
+	Generator<load_result> load(std::string path) override;
 };
 }
